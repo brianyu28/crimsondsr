5a5229f4756ef275b351849fd03e998323abaea2
Brian Yu <brianyu28@gmail.com>
2016-10-25 15:24:09 -0400

    Fix typo

--- application.py
+++ application.py
@@ -9,6 +9,6 @@ def index():
     return 'Crimson Daily Story Report'
 
 if __name__ == "__main__":
-    port = int(os.envion.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
