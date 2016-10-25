967334300c65fd5b160e5eb19c1cb58237a65db0
Brian Yu <brianyu28@gmail.com>
2016-10-25 14:54:05 -0400

    Set application port

--- application.py
+++ application.py
@@ -1,8 +1,14 @@
 from flask import Flask
 
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Crimson Daily Story Report'
+
+if __name__ == "__main__":
+    port = int(os.envion.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
